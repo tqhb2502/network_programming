7b592f79edc29641bec3fc8d3591d38b7e53bbc1
tqhb2502 <tqhb2502@gmail.com>
2022-10-22 19:38:45 +0700

    HW2 completed

--- HW2/resolver.c
+++ HW2/resolver.c
@@ -3,37 +3,75 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main () {
-    int i;
+/**
+ * Kiểm tra xem sâu ký tự có phải địa chỉ IP không
+ * Nếu là địa chỉ IP : trả về 1
+ * Nếu không phải: trả về 0
+*/
+int is_IP_address(char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+        if ((str[i] < '0' || '9' < str[i]) && str[i] != '.') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+
     struct hostent *he;
     struct in_addr **addr_list;
     struct in_addr addr;
 
-    // get the addresses of www.yahoo.com:
+    if (is_IP_address(argv[1])) {
+        
+        // nếu là địa chỉ IP
+        
+        // biến đổi địa chỉ IP từ dạng "số và dấu chấm" thành dạng nhị phân
+        inet_aton(argv[1], &addr);
 
-    he = gethostbyname("www.yahoo.com");
-    if (he == NULL) { // do some error checking
-        herror("gethostbyname"); // herror(), NOT perror()
-        exit(1);
-    }
+        // tìm kiếm thông tin của địa chỉ IP
+        he = gethostbyaddr(&addr, sizeof(addr), AF_INET);
 
-    // print information about this host:
-    printf("Official name is: %s\n", he->h_name);
-    printf("IP address: %s\n", inet_ntoa(*(struct in_addr*)he->h_addr));
-    printf("All addresses: ");
-    addr_list = (struct in_addr **)he->h_addr_list;
-    for(i = 0; addr_list[i] != NULL; i++) {
-        printf("%s ", inet_ntoa(*addr_list[i]));
-    }
-    printf("\n");
+        // nếu không tìm thấy
+        if (he == NULL) {
+            printf("Not found information\n");
+            exit(1);
+        }
 
-    // get the host name of 66.94.230.32:
+        // ngược lại, in thông tin
+        printf("Official name: %s\n", he->h_name);
+        if (he->h_aliases[0] != NULL) {
+            printf("Alias name:\n");
+            for (int i = 0; he->h_aliases[i] != NULL; i++) {
+                printf("%s\n", he->h_aliases[i]);
+            }
+        }
+    } else {
 
-    inet_aton("66.94.230.32", &addr);
-    he = gethostbyaddr(&addr, sizeof(addr), AF_INET);
-    if (he) {
-        printf("Host name: %s\n", he->h_name);
+        // nếu là tên miền
+
+        // tìm kiếm thông tin của tiên miền
+        he = gethostbyname(argv[1]);
+
+        // nếu không tìm thấy
+        if (he == NULL) {
+            printf("Not found information\n");
+            exit(1);
+        }
+
+        // ngược lại, in thông tin
+        addr_list = (struct in_addr **)he->h_addr_list;
+        printf("Official IP: %s\n", inet_ntoa(*addr_list[0]));
+        if (addr_list[1] != NULL) {
+            printf("Alias IP:\n");
+            for (int i = 1; addr_list[i] != NULL; i++) {
+                printf("%s\n", inet_ntoa(*addr_list[i]));
+            }
+        }
     }
 
     return 0;
